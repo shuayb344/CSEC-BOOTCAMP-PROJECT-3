9f11164c8e3bc5cc282b42963789280bd08bf50b
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2025-12-18 22:13:23 +0300

    making it responsive

--- toggle.js
+++ toggle.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('.btn');
 const container = document.querySelector('.js-container');
 const h1Text = document.querySelector('.js-h1');
+let isOn = false;
 btn.addEventListener('click',()=>{
-  if(h1Text.innerHTML === 'off'){
-    h1Text.innerHTML = 'on';
-    container.classList.add('container2');
-  }else{
+  if(isOn){
     h1Text.innerHTML = 'off';
     container.classList.remove('container2');
+  }else{
+    h1Text.innerHTML = 'on';
+    container.classList.add('container2');
   }
+  isOn =!isOn;
 });
